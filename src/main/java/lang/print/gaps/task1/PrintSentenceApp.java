c279fbf3e9042c3db067603842c45d6397ab0afd
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-21 15:16:51 +0300

    Update PrintSentenceApp.java

--- src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
+++ src/main/java/lang/print/gaps/task1/PrintSentenceApp.java
@@ -1,7 +1,7 @@
 package lang.print.gaps.task1;
 
 public class PrintSentenceApp {
-    public static void main(String[] args) {This is my first java program
-        System.out.print("This \n is \n my \n first \n java \n program");
+    public static void main(String[] args) {
+        System.out.println("This \n is \n my \n first \n java \n program");
     }
 }
