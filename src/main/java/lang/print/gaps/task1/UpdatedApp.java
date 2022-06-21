13b368246df05000ed0fb89b8013c3127fe6ab57
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-21 14:59:36 +0300

    Update UpdatedApp.java

--- src/main/java/lang/print/gaps/task1/UpdatedApp.java
+++ src/main/java/lang/print/gaps/task1/UpdatedApp.java
@@ -1,7 +1,8 @@
 package lang.print.gaps.task1;
 
 public class UpdatedApp {
-    public static void main(String[] args) {
-        System.out.println("updated");
+    public static void main(String[] args) {"I'm updated" + "\n" + "can be compiled. And can write to console"
+        System.out.println("I'm" + "\n" +"updated" + "\n" + "can" + "\n" + "be" + "\n" + "compiled." 
+                           + "\n" + "And" + "\n" + "can " + "\n" + "write" + "\n" + "to" + "\n" + "console");
     }
 }
