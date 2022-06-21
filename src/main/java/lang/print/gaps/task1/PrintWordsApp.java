09583bfa274e8a87f0d0d94141fb61bb9371c0cb
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-06-21 15:24:33 +0300

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -2,5 +2,5 @@ package lang.print.gaps.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println(""java" + \n "is" + \n +"smth");
+        System.out.println("java \nis \nsmth");
 }
